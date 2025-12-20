803aa4fcf7635ca4633e5bc64c9cab9b55047de0
IzayoiSakuye <ischh.zhang@outlook.com>
2025-12-20 10:17:34 +0800

    add api
    add Codeforces API to contest.js, now the page can display Codeforces's contests

--- js/contest.js
+++ js/contest.js
@@ -1,15 +1,3 @@
-// 实现获取最近比赛
-function getRecentContest(contests){
-  let earliest = null;
-  for (let contest of contests){
-    // console.log(contest);
-    if (earliest===null || contest.startTimeStamp<earliest.startTimeStamp){
-      earliest = contest;
-    }
-  }
-  return earliest;
-}
-
 // 实现悬停变色
 function hoverColor(elementId){
   // 按id获取容器
@@ -39,6 +27,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // 默认隐藏“正在进行”的比赛容器
   runningContestContainer.style.display = 'none';
+  recentContestContainer.style.display = 'none';
 
   /**
    * 直接使用API接口，控制台会显示错误：
@@ -48,7 +37,11 @@ document.addEventListener('DOMContentLoaded', function() {
    */
   const apiUrl = 'https://algcontest.rainng.com';
   const proxyUrl = 'https://api.allorigins.win/get?url=' + encodeURIComponent(apiUrl);
+  const cfApiUrl = 'https://codeforces.com/api/contest.list';
 
+  let errorFlagDom = false;
+  let errorFlagOvse = false;
+  // 牛客与力扣的比赛
   fetch(proxyUrl)
     .then(response => {
       if (!response.ok) {
@@ -60,11 +53,14 @@ document.addEventListener('DOMContentLoaded', function() {
 
       const contests = JSON.parse(data.contents);
       const nowTimeStamp = Date.now() / 1000;
+      const filterContest = contests.filter(contest =>{
+            return contest.oj === 'NowCoder';
+      });
 
       let runningContests = [];
       let upcomingContests = [];
       // 将读取到的比赛进行分类
-      contests.forEach(contest => {
+      filterContest.forEach(contest => {
         if (nowTimeStamp >= contest.startTimeStamp && nowTimeStamp <= contest.endTimeStamp) {
           runningContests.push(contest);
         } else if (contest.startTimeStamp > nowTimeStamp) {
@@ -74,11 +70,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
       // 正在进行的比赛
       if (runningContests.length > 0) {
-        const runningTitle = document.createElement('h2');
-        runningTitle.className = 'running-title';
-        runningTitle.innerText = "正在进行";
-        runningContestContainer.appendChild(runningTitle);
-        runningContestContainer.style.display = 'block'; // 显示容器
+        if (!runningContestContainer.querySelector('.running-title')){
+          const runningTitle = document.createElement('h2');
+          runningTitle.className = 'running-title';
+          runningTitle.innerText = "正在进行";
+          runningContestContainer.appendChild(runningTitle);
+          runningContestContainer.style.display = 'block'; // 显示容器
+        }
         runningContests.forEach(contest => {
           const contestElement = document.createElement('div');
           contestElement.className = "running-contest-item";
@@ -99,28 +97,33 @@ document.addEventListener('DOMContentLoaded', function() {
         if (runningContests.length == 0)
         {
           // 显示最近的一场比赛
-          const recentContest = getRecentContest(contests);
-          const recentTitle = document.createElement('h2');
-          recentTitle.className = 'recent-title';
-          recentTitle.innerText = "即将到来";
-          recentContestContainer.appendChild(recentTitle);
-          const recentContestElement = document.createElement('div');
-          recentContestElement.className = "recent-contest-item";
-          recentContestElement.innerHTML = `
+          if (!recentContestContainer.querySelector('.recent-title')){
+            const recentTitle = document.createElement('h2');
+            recentTitle.className = 'recent-title';
+            recentTitle.innerText = "即将到来";
+            recentContestContainer.appendChild(recentTitle);
+            recentContestContainer.style.display = 'block';
+          }
+        
+          const recentContest = upcomingContests[0];
+          const contestElement = document.createElement('div');
+          contestElement.className = "recent-contest-item";
+          contestElement.innerHTML = `
               <h2><a href="${recentContest.link}" target="_blank">${recentContest.name}</a></h2>
               <p><strong>平台: </strong> ${recentContest.oj}</p>
               <p><strong>开始时间: </strong> ${new Date(recentContest.startTime).toLocaleString()}</p>
               <p><strong>结束时间: </strong> ${new Date(recentContest.endTime).toLocaleString()}</p>
               <br>
             `;
-          recentContestContainer.appendChild(recentContestElement);
+          recentContestContainer.appendChild(contestElement);
         }
         
-
-        const futureTitle = document.createElement('h2');
-        futureTitle.className = 'future-title';
-        futureTitle.innerText = "未来比赛";
-        contestContainer.appendChild(futureTitle);
+        if (!contestContainer.querySelector('.future-title')){
+          const futureTitle = document.createElement('h2');
+          futureTitle.className = 'future-title';
+          futureTitle.innerText = "未来比赛";
+          contestContainer.appendChild(futureTitle);
+        }
         upcomingContests.forEach(contest => {
           const contestElement = document.createElement('div');
           contestElement.className = "contest-item";
@@ -134,15 +137,135 @@ document.addEventListener('DOMContentLoaded', function() {
           contestContainer.appendChild(contestElement);
         });
       } else {
-        recentContestContainer.innerHTML = '<p>没有即将到来的比赛。</p>';
-        contestContainer.innerHTML = '<p>没有即将到来的比赛。</p>';
+        recentContestContainer.innerHTML = '<p>没有即将到来的牛客比赛。</p>';
+        contestContainer.innerHTML = '<p>没有即将到来的牛客比赛。</p>';
       }      
     })
     .catch(error => {
       console.error('There has been a problem with your fetch operation:', error);
-      contestContainer.innerHTML = '<p>加载比赛信息失败，请稍后再试。</p>';
+      errorFlagDom = true;
     });
+    // cf的比赛
+    fetch(cfApiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        if (data.status!=='OK'){
+          contestContainer.innerHTML = '<p>获取比赛失败，请稍后再试。</p>'
+        }
+        else{
+          // 筛选未开始与正在进行的比赛
+          const filterContest = data.result.filter(contest =>{
+            return contest.phase === 'BEFORE' || contest.phase === 'CODING';
+          });
+
+          let runningContests = [];
+          let upcomingContests = [];
+          
+          filterContest.forEach(contest =>{
+            if (contest.relativeTimeSeconds<0){
+              upcomingContests.push(contest);
+            }
+            else{
+              runningContest.push(contest);
+            }
+          });
+          upcomingContests.reverse();
+
+          // 正在进行的比赛
+          if (runningContests.length>0){
+            if (!runningContestContainer.querySelector('.running-title')){
+              const runningTitle = document.createElement('h2');
+              runningTitle.className = 'running-title';
+              runningTitle.innerText = "正在进行";
+              runningContestContainer.appendChild(runningTitle);
+              runningContestContainer.style.display = 'block'; // 显示容器
+            }
+            
+            runningContests.forEach(contest => {
+              const contestElement = document.createElement('div');
+              const contestLink = `https://codeforces.com/contest/${contest.id}`
+              contestElement.className = "running-contest-item";
+              contestElement.innerHTML = `
+                <h2><a href="${contestLink}" target="_blank">${contest.name}</a></h2>
+                <p><strong>平台: </strong> Codeforces</p>
+                <p><strong>开始时间: </strong> ${new Date(contest.startTimeSeconds*1000).toLocaleString()}</p>
+                <p><strong>结束时间: </strong> ${new Date((contest.startTimeSeconds+contest.durationSeconds)*1000).toLocaleString()}</p>
+                <br>
+              `;
+              runningContestContainer.appendChild(contestElement);
+            });
+          }
+          
+          // 即将到来的比赛
+          if (upcomingContests.length > 0) {
+            // 防止重复
+            if (runningContests.length == 0)
+            {
+              // 显示最近的一场比赛
+                
+              if (!recentContestContainer.querySelector('.recent-title')){
+                const recentTitle = document.createElement('h2');
+                recentTitle.className = 'recent-title';
+                recentTitle.innerText = "即将到来";
+                recentContestContainer.appendChild(recentTitle);
+                recentContestContainer.style.display = 'block';
+              }
+              const recentContest = upcomingContests[0];
+              const contestElement = document.createElement('div');
+              const contestLink = `https://codeforces.com/contests`;
+              contestElement.className = "recent-contest-item";
+              contestElement.innerHTML = `
+                <h2><a href="${contestLink}" target="_blank">${recentContest.name}</a></h2>
+                <p><strong>平台: </strong> Codeforces</p>
+                <p><strong>开始时间: </strong> ${new Date(recentContest.startTimeSeconds*1000).toLocaleString()}</p>
+                <p><strong>结束时间: </strong> ${new Date((recentContest.startTimeSeconds+recentContest.durationSeconds)*1000).toLocaleString()}</p>
+                <br>
+              `;
+              recentContestContainer.appendChild(contestElement);
+            }
+            if (!contestContainer.querySelector('.future-title')){
+              const futureTitle = document.createElement('h2');
+              futureTitle.className = 'future-title';
+              futureTitle.innerText = "未来比赛";
+              contestContainer.appendChild(futureTitle);
+            }
+            
+            upcomingContests.forEach(contest => {
+              const contestElement = document.createElement('div');
+              const contestLink = `https://codeforces.com/contests`;
+              contestElement.className = "contest-item";
+              contestElement.innerHTML = `
+                <h2><a href="${contestLink}" target="_blank">${contest.name}</a></h2>
+                <p><strong>平台: </strong> Codeforces</p>
+                <p><strong>开始时间: </strong> ${new Date(contest.startTimeSeconds*1000).toLocaleString()}</p>
+                <p><strong>结束时间: </strong> ${new Date((contest.startTimeSeconds+contest.durationSeconds)*1000).toLocaleString()}</p>
+                <br>
+              `;
+              contestContainer.appendChild(contestElement);
+            });
+          } else {
+            recentContestContainer.innerHTML = '<p>没有即将到来的CF比赛。</p>';
+            contestContainer.innerHTML = '<p>没有即将到来的CF比赛。</p>';
+          }
+        }
+
+      })
+      .catch(error => {
+        console.error('There has been a problem with your fetch operation:', error);
+        errorFlagOvse = true;
+      });
+    
+    if (errorFlagDom && errorFlagOvse){
+      contestContainer.innerHTML = '<p>获取比赛失败，请稍后再试。</p>'
+    }
 });
+
+
 
 hoverColor('show-running-contest');
 hoverColor('show-recent-contest')
